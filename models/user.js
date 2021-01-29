ae8bf51bd706e53554e319a5e0a1499eaa6d27fc
= <za.qa@outlook.com>
2021-01-29 12:43:02 +0000

    safe keeping

--- models/user.js
+++ models/user.js
@@ -12,7 +12,8 @@ async function createUser(value) {
   const res = query(
     `
     INSERT INTO users (admin, name, surname, email, profileImage, cohort, currentRole, currentEmployer, skills, introduction, social)
-    VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
+      VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
+    RETURNING *
     `,
     [
       value.admin,
